f3fadceb0ff9e857a3ec548dc887dcec1ba0d5a0
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-04-29 23:47:30 +0600

    Added api creation in next js

---
+++ src/app/api/blogs/route.js
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+// Get API
+export const GET = () => {
+  return NextResponse.json({ message: "Hello, world!" });
+};
+
+// Post API
+
+export const POST = () => {
+  return NextResponse.json({ message: "Hello, world!" });
+};
+
+// Put API
+
+export const PUT = () => {
+  return NextResponse.json({ message: "Hello, world!" });
+};
+
+// Delete API
+
+export const DELETE = () => {
+  return NextResponse.json({ message: "Hello, world!" });
+};
+
+// Patch API
+export const PATCH = () => {
+  return NextResponse.json({ message: "Hello, world!" });
+};
