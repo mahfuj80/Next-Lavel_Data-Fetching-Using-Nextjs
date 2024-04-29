5775a55c89a8add764d5d51f7f8454a5eb772edb
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-04-29 19:44:27 +0600

    Added fetched data revalidate in porduction mode

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const res = await fetch("http://localhost:5000/posts", {
-  cache: "force-cache",
+  next: {
+    revalidate: 5,
+  },
 });
 const posts = await res.json();
 // console.log(posts);
